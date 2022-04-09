9d411c70c01f291e8b85372ad2fa4564c0170ad2
Adrian Delgado <ajdm_3000@hotmail.com>
2022-04-09 15:59:52 -0500

    Health check

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-hello-world
-//! ```
-
-use axum::{response::Html, routing::get, Router};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/health_check", get(health_check));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -21,6 +15,4 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
-}
+async fn health_check() {}
