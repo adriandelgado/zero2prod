9de1def1af239a225d770f8c63ba1cd0b95f1b1f
Adrian Delgado <ajdm_3000@hotmail.com>
2022-04-10 21:42:16 -0500

    Implement telemetry and secrecy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::SocketAddr;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{configuration::get_configuration, startup::run, telemetry};
 
 #[tokio::main]
 async fn main() {
-    let configuration = get_configuration().expect("Failed to read configuration file.");
+    let tracing_subscriber =
+        telemetry::get_tracing_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    telemetry::init_tracing_subscriber(tracing_subscriber);
 
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let configuration = get_configuration().expect("Failed to read configuration file.");
+    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect with specified Postgresql database.");
 
     let addr = SocketAddr::from(([127, 0, 0, 1], configuration.application_port));
-    println!("listening on http://{}", addr);
+    tracing::info!("listening on {addr}");
     run(&addr, connection).await;
 }
